934959e4b93a81ca0c6bd71d13daaed079cd592c
roshaka <roshaka@gmail.com>
2023-03-16 21:44:56 +0000

    update

--- src/masq.py
+++ src/masq.py
@@ -4,23 +4,25 @@ def masq(*target_keys, masq_char='*', masq_length=3):
     '''Decorate a func that returns a dictionary to mask target key's value'''
     def wrapper_2(func):
         def wrapper_1():
-            target_dict = func()
-            if not len(target_keys):
-                return target_dict
-            keys = target_dict.keys()
-            new_dict={}
-            for key in keys:
-                if key in target_keys:
-                    masqed_value = _generate_masq_string(
-                        target_dict[key],
-                        masq_char,
-                        masq_length,
-                    )
-                    new_dict[key]= masqed_value
-                else: new_dict[key]=target_dict[key]
-            return new_dict
+            return _masq_dict(target_keys, func(), masq_char, masq_length)
         return wrapper_1
     return wrapper_2
+
+def _masq_dict(target_keys, target_dict, masq_char='*', masq_length=3):
+    if not len(target_keys):
+        return target_dict
+    keys = target_dict.keys()
+    new_dict={}
+    for key in keys:
+        if key in target_keys:
+            masqed_value = _generate_masq_string(
+                target_dict[key],
+                masq_char,
+                masq_length,
+            )
+            new_dict[key]= masqed_value
+        else: new_dict[key]=target_dict[key]
+    return new_dict
 
 def _generate_masq_string(value, masq_char='*', masq_length=3):
     masq_length = masq_length if masq_length >=0 else len(value)
